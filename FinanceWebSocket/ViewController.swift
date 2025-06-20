6ef433d406ade3aec7835c88cedf92908cb75fde
sametkorkmaaz <smt.krkmz.2002@gmail.com>
2025-06-20 22:20:43 +0300

    Initial Commit

---
+++ FinanceWebSocket/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinanceWebSocket
+//
+//  Created by Samet Korkmaz on 20.06.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
